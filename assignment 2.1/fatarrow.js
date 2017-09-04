f348602f252ed224f5517b34b6e4c9965ebf711a
anuragassignment <seedboxtp7@gmail.com>
2017-09-05 00:50:32 +0530

    files added

---
+++ assignment 2.1/fatarrow.js
@@ -0,0 +1,4 @@
+//fat arrow funtion of type string takes string and number as input and prints result to console
+var fatArrow = function (string, number) { return ("string is " + string + ". the number given is " + number); };
+var output = fatArrow("string", 3);
+console.log(output);
---
+++ assignment 2.1/fatarrow.ts
@@ -0,0 +1,5 @@
+let fatArrow = (string: string, number: number): string => (`string is ${string}. the number given is ${number}`);
+
+let output = fatArrow("string",3);
+
+console.log(output);
